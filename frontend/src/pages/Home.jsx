27f69da55171060c2cf161c727a35c2ed2ae800c
zntb <zenetibi@yahoo.com>
2024-12-10 14:32:23 +0200

    Add Appdownload component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from '../components/Header/Header';
 import ExploreMenu from '../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../components/AppDownload/AppDownload';
 
 const Home = () => {
   const [category, setCategory] = useState('All');
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
