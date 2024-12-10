1a567dba72ce48d3b0359f07d6978d6ba5b1aab9
zntb <zenetibi@yahoo.com>
2024-12-10 14:23:04 +0200

    Add addTocart function

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -2,7 +2,35 @@ import cartModel from '../models/cart.js';
 import userModel from '../models/user.js';
 
 // add items to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (!user) {
+      return res.json({ success: false, message: 'User not found' });
+    }
+
+    const cart = await cartModel.findOne({ userId: user._id });
+    if (!cart) {
+      const newCart = new cartModel({
+        userId: user._id,
+        foodId: req.body.id,
+        quantity: 1,
+      });
+      await newCart.save();
+    } else {
+      const newCart = new cartModel({
+        userId: user._id,
+        foodId: req.body.id,
+        quantity: cart.quantity + 1,
+      });
+      await newCart.save();
+    }
+    res.json({ success: true, message: 'Item added to cart' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Error adding item to cart' });
+  }
+};
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {};
